115f1f8ba48ba67819b731a14a85f7af0766562a
asharrafli <muhamad21ti@mahasiswa.pcr.ac.id>
2025-06-14 16:20:56 +0700

    fourth commit

--- app/Models/TicketCategory.php
+++ app/Models/TicketCategory.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class TicketCategory extends Model
 {
-    //
+    protected $fillable = [
+        'name'
+    ];
+    public function tickets(){
+        return $this->hasMany(Tickets::class, 'category_id');
+    }
 }
